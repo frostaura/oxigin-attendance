407f5f4d6664921ab26c967b6d4047919fc6fc8d
Dean Martin <dean.martin@frostaura.net>
2025-06-02 22:04:17 +0200

    refactor

--- src/frontend/src/pages/ClientHome.tsx
+++ src/frontend/src/pages/ClientHome.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Layout, Card, Button, Table, Checkbox, message } from "antd";
 import { useNavigate } from "react-router-dom";
 import type { ColumnsType } from "antd/es/table";
-import { getJobRequestsAsync as getJobsAsync, getJobsRequiringApprovalAsync } from "../services/data/jobRequests";
+import { getJobRequestsAsync as getJobsAsync, getJobsRequiringApprovalAsync } from "../services/data/job";
 import type { Job } from "../models/jobModels";
 
 const { Header, Content } = Layout;
