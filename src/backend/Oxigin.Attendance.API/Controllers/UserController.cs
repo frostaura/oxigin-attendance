4b11530203e4babf1cfae2cfdf4065a945074c5f
Dean Martin <dean.martin@frostaura.net>
2025-06-02 09:40:58 +0200

    chore(Docs): Adding more comments.

--- src/backend/Oxigin.Attendance.API/Controllers/UserController.cs
+++ src/backend/Oxigin.Attendance.API/Controllers/UserController.cs
@@ -8,18 +8,35 @@ using Oxigin.Attendance.Shared.Models.Responses;
 
 namespace Oxigin.Attendance.API.Controllers;
 
+/// <summary>
+/// Controller for user-related actions such as sign in, sign up, and password changes.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class UserController : BaseController
 {
+    /// <summary>
+    /// The user manager service for handling user operations.
+    /// </summary>
     private readonly IUserManager _userManager;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserController"/> class.
+    /// </summary>
+    /// <param name="userManager">The user manager service.</param>
+    /// <param name="logger">The logger instance.</param>
     public UserController(IUserManager userManager, ILogger<UserController> logger)
         : base(logger)
     {
         _userManager = userManager;
     }
 
+    /// <summary>
+    /// Signs in a user with the provided credentials.
+    /// </summary>
+    /// <param name="request">The credentials for sign in.</param>
+    /// <param name="token">Cancellation token.</param>
+    /// <returns>User sign-in response or error.</returns>
     [HttpPost("SignIn")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserSigninResponse))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StandardizedError))]
@@ -28,6 +45,7 @@ public class UserController : BaseController
         try
         {
             var response = await _userManager.SignInAsync(request, token);
+
             return Ok(response);
         }
         catch (StandardizedErrorException e)
@@ -36,6 +54,12 @@ public class UserController : BaseController
         }
     }
 
+    /// <summary>
+    /// Registers a new user.
+    /// </summary>
+    /// <param name="user">The user to register.</param>
+    /// <param name="token">Cancellation token.</param>
+    /// <returns>User sign-in response or error.</returns>
     [HttpPost("SignUp")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserSigninResponse))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StandardizedError))]
@@ -44,6 +68,7 @@ public class UserController : BaseController
         try
         {
             var response = await _userManager.SignUpAsync(user, token);
+
             return Ok(response);
         }
         catch (StandardizedErrorException e)
@@ -52,7 +77,12 @@ public class UserController : BaseController
         }
     }
 
-    /// <returns>An IActionResult indicating success or failure.</returns>
+    /// <summary>
+    /// Changes the password for a user.
+    /// </summary>
+    /// <param name="request">The credentials for password change.</param>
+    /// <param name="token">Cancellation token.</param>
+    /// <returns>User sign-in response or error.</returns>
     [HttpPost("ChangePassword")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserSigninResponse))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StandardizedError))]
@@ -61,7 +91,6 @@ public class UserController : BaseController
         try
         {
             var response = await _userManager.ChangePasswordAsync(request, token);
-
             return Ok(response);
         }
         catch (StandardizedErrorException e)
