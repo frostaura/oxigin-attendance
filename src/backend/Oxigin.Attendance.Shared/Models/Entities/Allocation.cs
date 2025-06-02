7b3d01cca71b22cc24e3ca1515c6975beae0f133
Dean Martin <dean.martin@frostaura.net>
2025-06-02 23:15:44 +0200

    chore

--- src/backend/Oxigin.Attendance.Shared/Models/Entities/Allocation.cs
+++ src/backend/Oxigin.Attendance.Shared/Models/Entities/Allocation.cs
@@ -15,37 +15,31 @@ public class Allocation : BaseNamedEntity
     /// A brief description of the allocation taken from worker type.
     /// </summary>
     public string? Description { get; set; }
-
     /// <summary>
     /// The date and time of the allocation.
     /// </summary>
     [Required]
     public DateTime Time { get; set; }
-
     /// <summary>
     /// The number of hours needed for this allocation.
     /// </summary>
     [Required]
     public int HoursNeeded { get; set; }
-
     /// <summary>
     /// Foreign key to the Jobs table.
     /// Renamed to JobID.
     /// </summary>
     public Guid JobID { get; set; }
-
     /// <summary>
     /// Navigation property for the related Job.
     /// </summary>
     [ForeignKey(nameof(JobID))]
     public virtual Job Job { get; set; }
-
     /// <summary>
     /// Foreign key to the Employees table.
     /// Renamed to EmployeeID.
     /// </summary>
     public Guid EmployeeID { get; set; }
-
     /// <summary>
     /// Navigation property for the related Employee.
     /// </summary>
--- src/backend/Oxigin.Attendance.Shared/Models/Entities/Timesheet.cs
+++ src/backend/Oxigin.Attendance.Shared/Models/Entities/Timesheet.cs
@@ -22,41 +22,34 @@ public class Timesheet : BaseEntity
     /// Date and time of check-out.
     /// </summary>
     public DateTime TimeOut { get; set; }
-    
     /// <summary>
     /// Foreign key to the Jobs table.
     ///  Renamed to JobID.
     /// </summary>
     public Guid JobID { get; set; }
-
     /// <summary>
     /// Navigation property for the related Job.
     /// </summary>
     [ForeignKey(nameof(JobID))]
     public virtual Job Job { get; set; }
-    
     /// <summary>
     /// Foreign key to the Employee table.
     ///  Renamed to EmployeeID.
     /// </summary>
     public Guid EmployeeID { get; set; }
-
     /// <summary>
     /// Navigation property for the related Employee.
     /// </summary>
     [ForeignKey(nameof(EmployeeID))]
     public virtual Employee Employee { get; set; }
-    
     /// <summary>
     /// Foreign key to the SiteManager ID.
     ///  Renamed to SiteManagerID.
     /// </summary>
     public Guid SiteManagerID { get; set; }
-
     /// <summary>
     /// Navigation property for the related SiteManager.
     /// </summary>
     [ForeignKey(nameof(SiteManagerID))]
     public virtual User User { get; set; }
-
 }
